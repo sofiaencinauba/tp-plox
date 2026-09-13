5f3f0467a87fdd96e518f79a8ef5347953c6a1e6
tyreller <thiagoyair1@gmail.com>
2026-09-12 22:04:14 -0300

    feat: main con modo interprete y scanning parser wip

---
+++ src/main.rb
@@ -0,0 +1,58 @@
+require_relative 'scanner'
+require_relative 'parser'
+require_relative 'interpreter'
+
+class Rlox
+  attr_accessor :mode
+
+  def initialize
+    @mode = nil
+    @interpreter = Interpreter.new
+  end
+
+  def run(source)
+    scanner = Scanner.new(source)
+    tokens = scanner.scan
+
+    if @mode == :scanning
+      tokens.each do |token|
+        puts token.inspect unless token.nil?
+      end
+      return
+    end
+    
+    parser = Parser.new(tokens)
+    statements = parser.parse
+
+    if @mode == :parsing
+        puts statements
+        return
+    end
+
+    @interpreter.interpret(statements)
+    rescue Scanner::Error => e
+        puts "Scanning Error: #{e}"
+    rescue Parser::Error => e
+        puts "Parsing Error: #{e}"
+    rescue Interpreter::Error => e
+        puts "Runtime Error: #{e}"
+    end
+  
+end
+
+rlox = Rlox.new
+
+if ARGV.include?('--scanning')
+  rlox.mode = :scanning
+elsif ARGV.include?('--parsing')
+  rlox.mode = :parsing
+end
+
+loop do
+  print "> "
+  source = STDIN.gets
+
+  break if source.nil?
+
+  rlox.run(source)
+end
