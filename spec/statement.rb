a8449f4149c88ae0b0402af8c24d94e08df36bff
sofiaencinauba <sencina@fi.uba.ar>
2026-09-16 17:39:52 -0300

    feat: clase statement y tests

---
+++ spec/statement.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require_relative '../src/statement'
+
+RSpec.describe Statement do
+    describe 'var declaration' do
+        it 'creates a variable declaration statement' do
+            var_decl = VarDeclarationStatement.new('x', AST::Literal.new(5))
+            expect(var_decl.name).to eq('x')
+            expect(var_decl.initializer).to be_a(AST::Literal)
+            expect(var_decl.initializer.value).to eq(5)
+        end
+    end
+    describe 'function declaration' do
+        it 'creates a function declaration statement' do
+            func_decl = FunctionDeclaration.new('myFunc', ['a', 'b'], [AST::Literal.new(1)])
+            expect(func_decl.name).to eq('myFunc')
+            expect(func_decl.params).to eq(['a', 'b'])
+            expect(func_decl.body).to be_an(Array)
+            expect(func_decl.body.first).to be_a(AST::Literal)
+            expect(func_decl.body.first.value).to eq(1)
+        end
+    end
+    describe 'expression statement' do
+        it 'creates an expression statement' do
+            expr_stmt = ExpressionStatement.new(AST::Literal.new(10))
+            expect(expr_stmt.expression).to be_a(AST::Literal)
+            expect(expr_stmt.expression.value).to eq(10)
+        end
+    end
+    describe 'print statement' do   
+        it 'creates a print statement' do
+            print_stmt = PrintStatement.new(AST::Literal.new('Hello'))
+            expect(print_stmt.expression).to be_a(AST::Literal)
+            expect(print_stmt.expression.value).to eq('Hello')
+        end
+    end
+    describe 'block statement' do
+        it 'creates a block statement' do
+            block_stmt = BlockStatement.new([AST::Literal.new(1), AST::Literal.new(2)])
+            expect(block_stmt.statements).to be_an(Array)
+            expect(block_stmt.statements.size).to eq(2)
+            expect(block_stmt.statements.first).to be_a(AST::Literal)
+            expect(block_stmt.statements.first.value).to eq(1)
+        end
+    end
+    describe 'return statement' do
+        it 'creates a return statement' do
+            return_stmt = ReturnStatement.new(AST::Literal.new(42))
+            expect(return_stmt.value).to be_a(AST::Literal)
+            expect(return_stmt.value.value).to eq(42)
+        end
+    end
+    describe 'if statement' do
+        it 'creates an if statement' do
+            if_stmt = IfStatement.new(AST::Literal.new(true), AST::Literal.new('then'), AST::Literal.new('else'))
+            expect(if_stmt.condition).to be_a(AST::Literal)
+            expect(if_stmt.condition.value).to eq(true)
+            expect(if_stmt.then_branch).to be_a(AST::Literal)
+            expect(if_stmt.then_branch.value).to eq('then')
+            expect(if_stmt.else_branch).to be_a(AST::Literal)
+            expect(if_stmt.else_branch.value).to eq('else')
+        end
+    end
+end
---
+++ src/statement.rb
@@ -0,0 +1,105 @@
+
+class Statement
+end
+
+class VarDeclaration < Statement
+    attr_reader :name, :initializer
+    
+    def initialize(name, initializer = nil)
+        @name = name
+        @initializer = initializer
+    end
+    
+    def to_s
+        "VarDeclaration: #{@name} = #{@initializer}"
+    end
+end
+
+class FunctionDeclaration < Statement
+    attr_reader :name, :params, :body
+    
+    def initialize(name, params, body)
+        @name = name
+        @params = params
+        @body = body
+    end
+    
+    def to_s
+        "FunctionDeclaration: #{@name}(#{@params.join(', ')}) { #{@body} }"
+    end
+end
+
+class ExpressionStatement < Statement
+    attr_reader :expression
+    
+    def initialize(expression)
+        @expression = expression
+    end
+    
+    def to_s
+        "ExpressionStatement: #{@expression}"
+    end
+end
+
+class PrintStatement < Statement
+    attr_reader :expression
+    
+    def initialize(expression)
+        @expression = expression
+    end
+    
+    def to_s
+        "PrintStatement: #{@expression}"
+    end
+end
+
+class BlockStatement < Statement
+    attr_reader :statements
+    
+    def initialize(statements)
+        @statements = statements
+    end
+    
+    def to_s
+        "BlockStatement: { #{@statements.map(&:to_s).join('; ')} }"
+    end
+end
+
+class ReturnStatement < Statement
+    attr_reader :value
+    
+    def initialize(value)
+        @value = value
+    end
+    
+    def to_s
+        "ReturnStatement: return #{@value}"
+    end
+end
+
+class IfStatement < Statement
+    attr_reader :condition, :then_branch, :else_branch
+    
+    def initialize(condition, then_branch, else_branch)
+        @condition = condition
+        @then_branch = then_branch
+        @else_branch = else_branch
+    end
+    
+    def to_s
+        "IfStatement: if #{@condition} then #{@then_branch} else #{@else_branch}"
+    end
+end
+
+class WhileStatement < Statement
+    attr_reader :condition, :body
+    
+    def initialize(condition, body)
+        @condition = condition
+        @body = body
+    end
+    
+    def to_s
+        "WhileStatement: while #{@condition} do #{@body}"
+    end
+end
